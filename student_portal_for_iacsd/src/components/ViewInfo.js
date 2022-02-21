4a9503cc8fd6d413341977634ab3870f3cd01539
Biotechnologyguy <shubhamvt121@gmail.com>
2022-02-22 01:37:47 +0530

    Worked Mainly on Dashboard

--- student_portal_for_iacsd/src/components/ViewInfo.js
+++ student_portal_for_iacsd/src/components/ViewInfo.js
@@ -1,7 +1,10 @@
-import React from 'react'
-
+import React from "react";
+import "../css/ViewInfo.css";
+import dp from "../images/image.jpeg";
 export default function ViewInfo() {
   return (
-    <div>ViewInfo</div>
-  )
+    <div>
+      Here we will show student Information
+    </div>
+  );
 }
